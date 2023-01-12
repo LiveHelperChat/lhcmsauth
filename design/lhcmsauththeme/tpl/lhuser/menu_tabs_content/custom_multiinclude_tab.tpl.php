a8974530e7508207f91877c3c8c3115535e4a6ec
Remigijus Kiminas <remdex@gmail.com>
2023-01-12 07:33:46 -0500

    Migrate to BS5

--- design/lhcmsauththeme/tpl/lhuser/menu_tabs_content/custom_multiinclude_tab.tpl.php
+++ design/lhcmsauththeme/tpl/lhuser/menu_tabs_content/custom_multiinclude_tab.tpl.php
@@ -1,7 +1,7 @@
 <div role="tabpanel" class="tab-pane" id="msoauth">
     <?php if (erLhcoreClassUser::instance()->hasAccessTo('lhmsoauth','use_admin')) : ?>
         <?php foreach (erLhcoreClassModelMSAuth::getList(array('filter' => array('completed' => 1, 'user_id' => erLhcoreClassUser::instance()->getUserID()))) as $loggedAccount) : ?>
-        <a class="btn btn-sm btn-danger mb-1 mr-1" ng-non-bindable href="<?php echo  erLhcoreClassDesign::baseurl('msoauth/remove')?>/<?php echo $loggedAccount->id?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/msauth','Logout');?> - <?php echo htmlspecialchars($loggedAccount->display_name . ' '. $loggedAccount->name . ' ' . $loggedAccount->surname . '[' . $loggedAccount->email . ']')?></a>
+        <a class="btn btn-sm btn-danger mb-1 me-1" ng-non-bindable href="<?php echo  erLhcoreClassDesign::baseurl('msoauth/remove')?>/<?php echo $loggedAccount->id?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/msauth','Logout');?> - <?php echo htmlspecialchars($loggedAccount->display_name . ' '. $loggedAccount->name . ' ' . $loggedAccount->surname . '[' . $loggedAccount->email . ']')?></a>
         <?php endforeach; ?>
     <?php endif; ?>
     <br/>
